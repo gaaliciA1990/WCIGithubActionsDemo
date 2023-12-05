1296c1e8a8b6afaaa57b80ed33f9364d428aa743
Alicia G. <32443614+gaaliciA1990@users.noreply.github.com>
2023-12-05 14:15:34 -0800

    applied spotless formatting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,22 +5,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        printNumbers(concatNumbersToList(args));
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    printNumbers(concatNumbersToList(args));
+  }
 
-    public static List<String> concatNumbersToList(String[] nums) {
-        List<String> numbers = new ArrayList<>();
-        numbers.addAll(Arrays.asList(nums));
-        return numbers;
-    }
+  public static List<String> concatNumbersToList(String[] nums) {
+    List<String> numbers = new ArrayList<>();
+    numbers.addAll(Arrays.asList(nums));
+    return numbers;
+  }
 
-    public static void printNumbers(List<String> numbers) {
-        System.out.println("This is a list of numbers: ");
+  public static void printNumbers(List<String> numbers) {
+    System.out.println("This is a list of numbers: ");
 
-        for (String number : numbers) {
-            System.out.println(number);
-        }
+    for (String number : numbers) {
+      System.out.println(number);
     }
+  }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,31 +1,30 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class MainTest {
 
-    @Test
-    void concatNumbersToListTest() {
-        // ARRANGE
-        String[] test = new String[5];
-        test[0] = "5";
-        test[1] = "10";
-        test[2] = "63";
-        test[3] = "2";
+  @Test
+  void concatNumbersToListTest() {
+    // ARRANGE
+    String[] test = new String[5];
+    test[0] = "5";
+    test[1] = "10";
+    test[2] = "63";
+    test[3] = "2";
 
-        List<String> result = new ArrayList<>();
+    List<String> result = new ArrayList<>();
 
-        // ACT
-        result = Main.concatNumbersToList(test);
+    // ACT
+    result = Main.concatNumbersToList(test);
 
-        // ASSERT
-        assertFalse(result.isEmpty());
-        assertEquals("10", result.get(1));
-    }
+    // ASSERT
+    assertFalse(result.isEmpty());
+    assertEquals("10", result.get(1));
+  }
 }
