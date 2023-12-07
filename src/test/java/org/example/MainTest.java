21dba86890bf014cef6d36def0b74c9b304d029e
Alicia G. <32443614+gaaliciA1990@users.noreply.github.com>
2023-12-07 10:42:55 -0800

    commenting out a test

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,81 +1,93 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-/** Test class for {@Main} */
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * Test class for {@Main}
+ */
 class MainTest {
 
-  /** Test the concatenation of an array of number strings to a list of number strings */
-  @Test
-  void concatNumbersToListTest() {
-    // ARRANGE
-    String[] test = new String[5];
-    test[0] = "5";
-    test[1] = "10";
-    test[2] = "63";
-    test[3] = "2";
 
-    List<String> result = new ArrayList<>();
+    /**
+     * Test the concatenation of an array of number strings
+     * to a list of number strings
+     */
+    @Test
+    void concatNumbersToListTest() {
+        // ARRANGE
+        String[] test = new String[5];
+        test[0] = "5";
+        test[1] = "10";
+        test[2] = "63";
+        test[3] = "2";
 
-    // ACT
-    result = Main.concatNumbersToList(test);
+        List<String> result = new ArrayList<>();
 
-    // ASSERT
-    assertFalse(result.isEmpty());
-    assertEquals("10", result.get(1));
-  }
+        // ACT
+        result = Main.concatNumbersToList(test);
 
-  /** Test the print numbers class prints a list of numbers Uses contains to verify our result */
-  @Test
-  void printNumbersTest() {
-    // ARRANGE
-    PrintStream mockedPS = Mockito.mock(PrintStream.class);
-    PrintStream old = System.out;
-    System.setOut(mockedPS);
+        // ASSERT
+        assertFalse(result.isEmpty());
+        assertEquals("10", result.get(1));
+    }
 
-    String[] test = new String[5];
-    test[0] = "5";
-    test[1] = "10";
-    test[2] = "63";
-    test[3] = "2";
+    /**
+     * Test the print numbers class prints a list of numbers
+     * Uses contains to verify our result
+     */
+//    @Test
+//    void printNumbersTest() {
+//        // ARRANGE
+//        PrintStream mockedPS = Mockito.mock(PrintStream.class);
+//        PrintStream old = System.out;
+//        System.setOut(mockedPS);
+//
+//        String[] test = new String[5];
+//        test[0] = "5";
+//        test[1] = "10";
+//        test[2] = "63";
+//        test[3] = "2";
+//
+//        String expected = "This is a list of numbers: ";
+//
+//        // ACT
+//        List<String> result = Main.concatNumbersToList(test);
+//        Main.printNumbers(result);
+//
+//        // ASSERT
+//        assertFalse(result.isEmpty());
+//        Mockito.verify(mockedPS).println(Mockito.contains(expected));
+//    }
 
-    String expected = "This is a list of numbers: ";
+    @Test
+    void mainTest() {
+        // ARRANGE
+        PrintStream mockedPS = Mockito.mock(PrintStream.class);
+        PrintStream old = System.out;
+        System.setOut(mockedPS);
 
-    // ACT
-    List<String> result = Main.concatNumbersToList(test);
-    Main.printNumbers(result);
+        String[] test = new String[5];
+        test[0] = "23";
+        test[1] = "5";
+        test[2] = "13";
+        test[3] = "64";
 
-    // ASSERT
-    assertFalse(result.isEmpty());
-    Mockito.verify(mockedPS).println(Mockito.contains(expected));
-  }
+        String expected = "Hello world!";
 
-  @Test
-  void mainTest() {
-    // ARRANGE
-    PrintStream mockedPS = Mockito.mock(PrintStream.class);
-    PrintStream old = System.out;
-    System.setOut(mockedPS);
+        // ACT
+        Main.main(test);
 
-    String[] test = new String[5];
-    test[0] = "23";
-    test[1] = "5";
-    test[2] = "13";
-    test[3] = "64";
-
-    String expected = "Hello world!";
-
-    // ACT
-    Main.main(test);
-
-    // ASSERT
-    Mockito.verify(mockedPS).println(Mockito.contains(expected));
-  }
+        // ASSERT
+        Mockito.verify(mockedPS).println(Mockito.contains(expected));
+    }
 }
