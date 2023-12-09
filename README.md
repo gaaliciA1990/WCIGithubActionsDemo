48dd43b66028bb854d873dca10bdf5aa9efc5e9b
Alicia G. <32443614+gaaliciA1990@users.noreply.github.com>
2023-12-08 16:47:17 -0800

    updating for testing

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # WCI Github Actions Demo
 
-## Reports
+## Workflow/GitHub Action Status
 [![Java CI with Gradle](https://github.com/gaaliciA1990/WCIGithubActionsDemo/actions/workflows/gradle.yml/badge.svg)](https://github.com/gaaliciA1990/WCIGithubActionsDemo/actions/workflows/gradle.yml)
 
-[![Code Coverage](https://github.com/gaaliciA1990/WCIGithubActionsDemo/blob/develop/.github/badges/jacoco.svg)](https://github.com/gaaliciA1990/WCIGithubActionsDemo/actions/workflows/jacoco.yml)
+[![JaCoCo Report](https://github.com/gaaliciA1990/WCIGithubActionsDemo/actions/workflows/jacoco.yml/badge.svg)](https://github.com/gaaliciA1990/WCIGithubActionsDemo/actions/workflows/jacoco.yml)
+
+[![Test Coverage](https://github.com/gaaliciA1990/WCIGithubActionsDemo/blob/develop/.github/badges/jacoco.svg)](https://github.com/gaaliciA1990/WCIGithubActionsDemo/actions/workflows/jacoco.yml)
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,93 +1,81 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-/**
- * Test class for {@Main}
- */
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+/** Test class for {@Main} */
 class MainTest {
 
+  /** Test the concatenation of an array of number strings to a list of number strings */
+  @Test
+  void concatNumbersToListTest() {
+    // ARRANGE
+    String[] test = new String[5];
+    test[0] = "5";
+    test[1] = "10";
+    test[2] = "63";
+    test[3] = "2";
 
-    /**
-     * Test the concatenation of an array of number strings
-     * to a list of number strings
-     */
-    @Test
-    void concatNumbersToListTest() {
-        // ARRANGE
-        String[] test = new String[5];
-        test[0] = "5";
-        test[1] = "10";
-        test[2] = "63";
-        test[3] = "2";
+    List<String> result = new ArrayList<>();
 
-        List<String> result = new ArrayList<>();
+    // ACT
+    result = Main.concatNumbersToList(test);
 
-        // ACT
-        result = Main.concatNumbersToList(test);
+    // ASSERT
+    assertFalse(result.isEmpty());
+    assertEquals("10", result.get(1));
+  }
 
-        // ASSERT
-        assertFalse(result.isEmpty());
-        assertEquals("10", result.get(1));
-    }
+  /** Test the print numbers class prints a list of numbers Uses contains to verify our result */
+  @Test
+  void printNumbersTest() {
+    // ARRANGE
+    PrintStream mockedPS = Mockito.mock(PrintStream.class);
+    PrintStream old = System.out;
+    System.setOut(mockedPS);
 
-    /**
-     * Test the print numbers class prints a list of numbers
-     * Uses contains to verify our result
-     */
-//    @Test
-//    void printNumbersTest() {
-//        // ARRANGE
-//        PrintStream mockedPS = Mockito.mock(PrintStream.class);
-//        PrintStream old = System.out;
-//        System.setOut(mockedPS);
-//
-//        String[] test = new String[5];
-//        test[0] = "5";
-//        test[1] = "10";
-//        test[2] = "63";
-//        test[3] = "2";
-//
-//        String expected = "This is a list of numbers: ";
-//
-//        // ACT
-//        List<String> result = Main.concatNumbersToList(test);
-//        Main.printNumbers(result);
-//
-//        // ASSERT
-//        assertFalse(result.isEmpty());
-//        Mockito.verify(mockedPS).println(Mockito.contains(expected));
-//    }
+    String[] test = new String[5];
+    test[0] = "5";
+    test[1] = "10";
+    test[2] = "63";
+    test[3] = "2";
 
-    @Test
-    void mainTest() {
-        // ARRANGE
-        PrintStream mockedPS = Mockito.mock(PrintStream.class);
-        PrintStream old = System.out;
-        System.setOut(mockedPS);
+    String expected = "This is a list of numbers: ";
 
-        String[] test = new String[5];
-        test[0] = "23";
-        test[1] = "5";
-        test[2] = "13";
-        test[3] = "64";
+    // ACT
+    List<String> result = Main.concatNumbersToList(test);
+    Main.printNumbers(result);
 
-        String expected = "Hello world!";
+    // ASSERT
+    assertFalse(result.isEmpty());
+    Mockito.verify(mockedPS).println(Mockito.contains(expected));
+  }
 
-        // ACT
-        Main.main(test);
+  @Test
+  void mainTest() {
+    // ARRANGE
+    PrintStream mockedPS = Mockito.mock(PrintStream.class);
+    PrintStream old = System.out;
+    System.setOut(mockedPS);
 
-        // ASSERT
-        Mockito.verify(mockedPS).println(Mockito.contains(expected));
-    }
+    String[] test = new String[5];
+    test[0] = "23";
+    test[1] = "5";
+    test[2] = "13";
+    test[3] = "64";
+
+    String expected = "Hello world!";
+
+    // ACT
+    Main.main(test);
+
+    // ASSERT
+    Mockito.verify(mockedPS).println(Mockito.contains(expected));
+  }
 }
